7af2e1a2bd78abcc8330f5e322a4c314828635e4
mihau07-creator <mihau07@gmail.com>
2026-04-22 09:10:10 +0200

    Automatyczna aktualizacja

--- github_upload.bat
+++ github_upload.bat
@@ -1,12 +1,12 @@
 @echo off
 cd /d "%~dp0"
-echo Konfigurowanie i wysylanie na GitHub (https://github.com/michallytka/baza-base.git)...
+echo Konfigurowanie i wysylanie na GitHub (https://github.com/mihau07-creator/baza-base.git)...
 
 :: Remove origin if it exists to ensure we set the new one correctly
 git remote remove origin 2>nul
 
 :: Add the remote
-git remote add origin https://github.com/michallytka/baza-base.git
+git remote add origin https://github.com/mihau07-creator/baza-base.git
 
 :: Rename branch to main just in case
 git branch -M main
